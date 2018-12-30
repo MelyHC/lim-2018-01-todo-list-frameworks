f63e712c7af624b92c2271020af12570c6459dd4
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-12-29 19:04:37 -0500

    Updates

---
+++ precache-manifest.2e1ed33e403ca916ff9d367442075bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50626c473dcd3c18512c",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/runtime~main.50626c47.js"
+  },
+  {
+    "revision": "4574e86b1271daa7415f",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/main.4574e86b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/lim-2018-01-todo-list-frameworks/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4574e86b1271daa7415f",
+    "url": "/lim-2018-01-todo-list-frameworks/static/css/main.5fab169c.chunk.css"
+  },
+  {
+    "revision": "abf7b399db7171d84825543d1eccfb04",
+    "url": "/lim-2018-01-todo-list-frameworks/index.html"
+  }
+];
